8bb6c14356e4594d24d2f81b4d38c030b409562d
Brian E Clow <brian.clow+cloud9@procore.com>
2017-08-06 23:45:50 +0000

    Disabling AWS/carrier_wave initializer

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory     =  ENV['S3_BUCKET']
-  end
-end
+# DISABLED # if Rails.env.production?
+# DISABLED #   CarrierWave.configure do |config|
+# DISABLED #     config.fog_credentials = {
+# DISABLED #       # Configuration for Amazon S3
+# DISABLED #       :provider              => 'AWS',
+# DISABLED #       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+# DISABLED #       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+# DISABLED #     }
+# DISABLED #     config.fog_directory     =  ENV['S3_BUCKET']
+# DISABLED #   end
+# DISABLED # end
